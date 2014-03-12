f484c37f33777ffdba1fde361d17a4c1d614a1a7
syounger <simcha@spondoolies-tech.com>
2014-03-12 13:28:09 +0200

    added firmware upgrade page which runs upgrade script. not fully tested, since no upgrade is script is set up yet

---
+++ http/constants.php
@@ -0,0 +1,4 @@
+<?php
+
+define('FIRMWARE_UPGRADE_SCRIPT', '/usr/local/bin/upgrade-software.sh');
+define('FIRMWARE_UPGRADE_PARAMS', '--url=http://spondoolies-tech.com/releases/spon.shar');
--- http/menu.php
+++ http/menu.php
@@ -1,3 +1,4 @@
+<?php require_once('constants.php');?>
 <div class="navbar">
   <div class="container">
     <a class="navbar-brand" href="">SpondMiner</a>
@@ -5,6 +6,9 @@
       <li><a href="/">Status</a></li>
       <li><a href="pools.php">Pool</a></li>
       <li><a href="settings.php">Settings</a></li>
+<? if(file_exists(FIRMWARE_UPGRADE_SCRIPT)){ ?>
+      <li><a href="firmware.php">Firmware Upgrade</a></li>
+<? } ?>
 <!--      <li><a href="about.php">About</a></li>
       <li><a href="contact.php">Contact</a></li>
 -->      <li><a href="license.php">License</a></li>
