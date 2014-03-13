bfed131966a14cc0e7cd9ff026c089eba773f430
Simcha <simcha@spondoolies-tech.com>
2014-03-13 12:20:56 +0200

    fixed url parameter

--- http/constants.php
+++ http/constants.php
@@ -1,4 +1,4 @@
 <?php
 
 define('FIRMWARE_UPGRADE_SCRIPT', '/usr/local/bin/upgrade-software.sh');
-define('FIRMWARE_UPGRADE_PARAMS', '--url=http://spondoolies-tech.com/releases/spon.shar');
+define('FIRMWARE_UPGRADE_PARAMS', '--url http://23.251.137.136/release/spon.shar');
