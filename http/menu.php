c100a413792593d72bc21cd52df939a70206122f
Simcha <simcha@spondoolies-tech.com>
2014-03-14 12:51:33 +0200

    Changed "Spondoolies" menu item to link to home, instead of current page

--- http/menu.php
+++ http/menu.php
@@ -1,7 +1,7 @@
 <?php require_once('constants.php');?>
 <div class="navbar">
   <div class="container">
-    <a class="navbar-brand" href="">SpondMiner</a>
+    <a class="navbar-brand" href="/">SpondMiner</a>
     <ul class="nav navbar-nav">
       <li><a href="/">Status</a></li>
       <li><a href="pools.php">Pool</a></li>
