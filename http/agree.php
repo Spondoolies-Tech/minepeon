73fca7342b87cb2ab87f72a53af2801f0af0d592
Simcha <simcha@spondoolies-tech.com>
2014-03-16 23:43:29 +0200

    require user to agree to terms and conditions

---
+++ http/agree.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * show the user agreement
+ * If the user has not yet agred, show a button to allow him to accept.
+ */
+
+require_once('settings.inc.php');
+$agreement = file_get_contents('inc/terms_and_conditions.txt');
+
+require_once('head.php');
+?>
+<div class="text-center">
+<div class="text-left text-info">
+<pre>
+<?=$agreement?>
+</pre>
+</div>
+<?php if(!array_key_exists('accept', $settings) || ! intval(time($settings['accept'])) ){ ?>
+<a id="agree" onclick="agree()" class="btn btn-info">Yes, I agree to the terms and conditions.</a>
+<script type="text/javascript">
+function agree(){
+// $('#agree').click(function(){
+	$(this).addClass('disabled');
+	$.post('settings.php', {agree:true}, function(){
+		document.location.reload();
+		});
+	return false;
+	} // );
+</script>
+<?php } ?>
+</div>
+
+<?php include('foot.php'); ?>
+
