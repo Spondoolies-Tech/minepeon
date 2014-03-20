62f30fa2fef958d6a2580ba9d22384872fe2c10d
Simcha <simcha@spondoolies-tech.com>
2014-03-20 15:14:41 +0200

    restructure, and a number of fixes

--- etc/cron.d/hourly/pandp_register.sh
+++ etc/cron.d/hourly/pandp_register.sh
@@ -1,68 +1,95 @@
 #!/bin/sh
 
-# functions
+# functions copied from vladik update
 no_bootsource=201
+mount_fail=204
+
+# Mount boot partition if not mounted already.
+mount_boot_partition()
+{
+	grep -q /mnt/${bootfrom}-boot /proc/mounts ||
+		mount /mnt/${bootfrom}-boot 2>/dev/null ||
+			{ echo "Cannot mount /mnt/${bootfrom}-boot"; exit ${mount_fail}; }
+}
+umount_boot_partition(){
+		umount /mnt/${bootfrom}-boot 2>/dev/null 
+}
+
 detect_boot_source()
 {
-# modified from Vladiks script to use /tmp/ if no bootsource found!!! 
 	# bootfrom=<BOOTSOURCE> is always the last element of kernel
 	# command line so may get it this way.
 	grep -q bootfrom /proc/cmdline &&
 		bootfrom=`sed 's/.*bootfrom=//' /proc/cmdline` 
 }
-detect_boot_source
 
 # get external ip
-
-ext_ip=`curl -s --head http://myexternalip.com/ | awk '/External-Ip/{print $2}'`
+external_ip(){
+	ext_ip=`curl -s --head http://myexternalip.com/ | awk '/External-Ip/{print $2}'`
+}
 
 # get lan ip
-lan_ip=`ifconfig | grep -A1 eth0 | awk '/inet/{print $2}' | cut -c 6-`
-if [ -z "$lan_ip" ]; then
-	lan_ip="NA"
-fi
-	
+lan_ip(){
+	lan_ip=`ifconfig | grep -A1 eth0 | awk '/inet/{print $2}' | cut -c 6-`
+	if [ -z "$lan_ip" ]; then
+		lan_ip="NA"
+	fi
+}	
 
 # get wan ip
-wan_ip=`ifconfig | grep -A1 wan0 | awk '/inet/{print $2}' | cut -c 6-`
-if [ -z "$wan_ip" ]; then
-	wan_ip="NA"
-fi
- 
+wan_ip(){
+	wan_ip=`ifconfig | grep -A1 wan0 | awk '/inet/{print $2}' | cut -c 6-`
+	if [ -z "$wan_ip" ]; then
+		wan_ip="NA"
+	fi
+}
 
-# get firmware version
-# instructions from Vladik
-firmware=`mkimage -l /mnt/${bootfrom}-boot/uImage | grep 'Image Name' | sed 's/.*:\s\+//'`
+board_id(){
+	EEPROM_DEVICE=/sys/bus/i2c/devices/0-0050/eeprom
+	board_id=`dd bs=12 skip=7 count=1 if=$EEPROM_DEVICE 2>/dev/null`
+}
 
-# get model number
-# script from Vladik
-EEPROM_DEVICE=/sys/bus/i2c/devices/0-0050/eeprom
-#board=`dd bs=12 skip=7 count=1 if=$EEPROM_DEVICE 2>/dev/null`
-board=`cat /board_ver`
-
-# 
+debug(){ 
 printf "External IP: %s\n" $ext_ip
 printf "Lan IP: %s\n" $lan_ip
 printf "WLan IP: %s\n" $wan_ip
-printf "Board Serial Number: %s\n" $board
 printf "Firmware: %s\n" $firmware
+printf "Model: %s\n" $board
+printf "Board ID: %s\n" $board_id
+}
 
- 
-s=`curl -s --include --header "Content-Type: application/json" \
-     --request PUT \
-     --data-binary "{
-    \"modelNumber\": \"ABCD12345\",
-    \"lanAddress\": \"$lan_ip\",
-    \"wanAddress\": \"$wan_ip\",
-    \"fwVersion\": \"$firmware\",
-    \"minerName\": \"$board\",
-}" \
-     "https://private-4745-spondapi.apiary.io/devices/registerDevice" \
-	| head -1 | awk '{print $2}'`
+send_data(){ 
+	s=`curl -s --include --header "Content-Type: application/json" \
+	     --request PUT \
+	     --data-binary "{
+	    \"modelNumber\": \"$board\",
+	    \"lanAddress\": \"$lan_ip\",
+	    \"wanAddress\": \"$wan_ip\",
+	    \"fwVersion\": \"$firmware\",
+	    \"boardID\": \"$board_id\",
+	}" \
+	     "https://private-4745-spondapi.apiary.io/devices/registerDevice" \
+		| head -1 | awk '{print $2}'`
+	debug
+}
 
-if [ $s=200 ]; then
-	echo "saved"
-else
-	printf "error registering machine, %s" $s | tee -a /var/log/messages
-fi
+main(){
+	detect_boot_source
+	mount_boot_partition
+	board=`cat /board_ver`
+	firmware=`cat /fw_ver`
+	board_id
+	external_ip
+	lan_ip
+	wan_ip
+	send_data
+	umount_boot_partition
+	if [ $s=200 ]; then
+		echo "saved"
+	else
+		printf "error registering machine, %s" $s | tee -a /var/log/messages
+	fi
+}
 
+main
+return $?
