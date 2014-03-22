24ae5e99dbe3d0d7acca8eb79bb5e5823f084882
Stas Oskin <stas.oskin@gmail.com>
2014-03-22 06:24:18 +0200

    Hidden status menu, and added open to new tab for logo

--- http/menu.php
+++ http/menu.php
@@ -2,7 +2,8 @@
   <div class="container">
     <a class="navbar-brand" href="/">SP10 Dawson</a>
     <ul class="nav navbar-nav">
-      <li><a href="/">Status</a></li>
+      <!--<li><a href="/">Status</a></li>-->
+      <!--<li><a href="/asicstats.php">ASIC Stats</a></li>-->
       <li><a href="/pools.php">Pools</a></li>
       <li><a href="/settings.php">Settings</a></li>
       <li><a href="/firmware.php">Firmware Upgrade</a></li>
@@ -10,7 +11,7 @@
       <li><a href="contact.php">Contact</a></li>
 -->      <li><a href="/license.php">License</a></li>
     </ul>
-	<a href="http://www.spondoolies-tech.com"><img class="logo responsive pull-right" src="img/SpondooliesLogo-transparent-small.png" /></a>
+	<a href="http://www.spondoolies-tech.com" target="_blank"><img class="logo responsive pull-right" src="img/SpondooliesLogo-transparent-small.png" /></a>
   </div>
 </div>
 
