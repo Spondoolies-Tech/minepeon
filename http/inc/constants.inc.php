2d73ae66cfb8c0fc5db5649b1e2d9278f0c51638
Stas Oskin <stas.oskin@gmail.com>
2014-03-23 12:57:44 +0200

    Added development firmware upgrade

--- http/inc/constants.inc.php
+++ http/inc/constants.inc.php
@@ -2,7 +2,7 @@
 
 const FIRMWARE_UPGRADE_SCRIPT = "/usr/local/bin/upgrade-software.sh";
 const FIRMWARE_UPGRADE_URL = "http://firmware.spondoolies-tech.com/release/spon.tar";
-//const FIRMWARE_UPGRADE_URL = "http://10.0.0.29:1337/release/spon.tar";
+const FIRMWARE_UPGRADE_URL_DEV = "http://firmware.spondoolies-tech.com/development/spon.tar";
 
 const UI_USER_NAME = "admin";
 
---
+++ http/upgrade_dev.php
@@ -0,0 +1,18 @@
+<?php
+require_once('constants.inc.php');
+header("Content-type: text/text");
+header("Transfer-Encoding: chunked");
+
+//no cache! copied from http://php.netmanual/en/function.header.php
+header('Expires: Sat 26 Jul 1997 05:00:00 GMT');
+header('Last-Modified: '.gmdate('D, d M Y H:i:i').' GMT');
+header('Cache-Control: no-store, no-cache, must-revalidate');
+header('Cache-control: post-check=0, pre-check=0', false);
+header('Pragma: no-cache');
+
+echo "Beginning upgrade. Please wait, this can take up to 1 minute.\n";
+ob_flush();
+flush();
+
+//Start the  process
+passthru(FIRMWARE_UPGRADE_SCRIPT . " --url " . FIRMWARE_UPGRADE_URL_DEV, $result);
