bebf1a77aba64ee99b0ab6d5e3e8f57d2535dee4
Simcha <simcha@spondoolies-tech.com>
2014-03-24 19:01:19 +0200

    fixed file name

--- http/inc/constants.inc.php
+++ http/inc/constants.inc.php
@@ -11,5 +11,5 @@ const DEFAULT_MINER_WORKMODE = 2; //turbo
 
 const MAX_ELECTRICAL_USAGE_FILE = '/etc/mg_psu_limit';
 
-const CURRENT_VERSION_FILE = '/fw_version';
+const CURRENT_VERSION_FILE = '/fw_ver';
 const LATEST_VERSION_FILE = '/tmp/fw_update';
