2a7ee95d2136b48321caf7215e38860ba8a8ce4f
Simcha <simcha@spondoolies-tech.com>
2014-03-26 20:05:07 +0200

    removed new firmware notice

--- http/menu.php
+++ http/menu.php
@@ -9,11 +9,12 @@ $latest_ver = exec('cat '.LATEST_VERSION_FILE);
       <!--<li><a href="/">Status</a></li>-->
       <li><a href="/pools.php">Pools</a></li>
       <li><a href="/settings.php">Settings</a></li>
-<?php if($cur_ver == $latest_ver){ ?>
+      <li><a href="/firmware.php" >Firmware Upgrade</a></li>
+<?php /* if($cur_ver == $latest_ver){ ?>
 	<li><span class="disabled" title="You already have the latest version of the firmware installed.">Firmware Upgrade</span></li>
 <?php }else{ ?>
       <li><a class="alert" href="/firmware.php" title="Upgrade to version <?php echo $latest_ver;?> (Current version is <?php echo $cur_ver?>.)" >Firmware Upgrade</a><span class="badge alert" style="position:absolute;top:4px;right:0;"><?php echo $latest_ver;?></span></li>
-<?php } ?>
+<?php } */ ?>
       <li><a href="/asics.php">ASIC Stats</a></li>
 <!--      <li><a href="about.php">About</a></li>
       <li><a href="contact.php">Contact</a></li>
