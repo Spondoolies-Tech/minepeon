6f66b5f91ad96ab4f1ddd0bb36fbfab46ee4e9fe
Simcha <simcha@spondoolies-tech.com>
2014-03-26 14:55:36 +0200

    removed debugging lines

--- http/inc/leds.inc.php
+++ http/inc/leds.inc.php
@@ -2,19 +2,16 @@
 require_once('inc/constants.inc.php');
 
 function led_on($led_id){
-	echo 'echo 1 > /sys/class/gpio/gpio'.$led_id.'/value';
 	exec('echo 1 > /sys/class/gpio/gpio'.$led_id.'/value');
 }
 
 function led_off($led_id){
-	echo 'echo 0 > /sys/class/gpio/gpio'.$led_id.'/value';
 	exec('echo 0 > /sys/class/gpio/gpio'.$led_id.'/value');
 }
 
 function led_flash($led_id, $flashes = 1, $duration=.2){
 	//in case its on, lets turn it off for a quick flash
 	$duration *= 1000000;
-	echo $duration;
 	led_off($led_id);
 	for($i = 0; $i < $flashes; $i++){
 		led_on($led_id);
