39fcdc24a1f6996a5b1009c530001163d119291f
Stas Oskin <stas.oskin@gmail.com>
2014-04-17 13:36:15 +0300

    Added error message of WiFi connection

--- http/wifi.php
+++ http/wifi.php
@@ -49,44 +49,55 @@ include('menu.php');
             post_to_url('/wifi.php', {connect: 'true', wifiName: wifiNameParam, keyMgmt: keyMgmtParam, protocol: protocolParam, pairWise: pairWiseParam, groupCiphers: groupCiphersParam});
         }
     }
+
+<?php
+if(isset($_GET["result"]) && $_GET["result"] != 0) {
+?>
+    window.onload = function () {
+        bootbox.alert("Error connecting to WiFi network!", function() {
+        });
+    };
+<?php
+}
+?>
 </script>
 
 <?php
-    //Scan for WiFi on every page refresh
-    //exec("iwlist wlan0 scan | iwlist-scan-parse.awk > " . WIFI_NETWORKS_FILE);
-    $wifiJson = file_get_contents(WIFI_NETWORKS_FILE);
-    $wifiNetworks = json_decode($wifiJson, true);
+//Scan for WiFi on every page refresh
+//exec("iwlist wlan0 scan | iwlist-scan-parse.awk > " . WIFI_NETWORKS_FILE);
+$wifiJson = file_get_contents(WIFI_NETWORKS_FILE);
+$wifiNetworks = json_decode($wifiJson, true);
 
-    //Handle the connect command
-    if (isset($_POST['connect'])) {
-        //Preparation
-        $desc = array(
-            0 => array("pipe", "r"),  // stdin is a pipe that the child will read from
-            1 => array("pipe", "w"),  // stdout is a pipe that the child will write to
-            2 => array("file", "/tmp/wifi-conf-create.txt", "a") // stderr is a file to write to
-        );
+//Handle the connect command
+if (isset($_POST['connect'])) {
+    //Preparation
+    $desc = array(
+        0 => array("pipe", "r"),  // stdin is a pipe that the child will read from
+        1 => array("pipe", "w"),  // stdout is a pipe that the child will write to
+        2 => array("file", "/tmp/wifi-conf-create.txt", "a") // stderr is a file to write to
+    );
 
-        //Create and open new process
-        $cmd = 'ESSID="'.$_POST['wifiName'].'" KEY_MGMT"'.$_POST['keyMgmt'].'" PROTO="'.$_POST['protocol'].'" PAIRWISE_CIPHERS="'.$_POST['pairWise'].'" GROUP_CIPHERS="'.$_POST['groupCiphers'].'" wifi-conf-create.sh';
-        $process = proc_open($cmd, $desc, $pipes);
+    //Create and open new process
+    $cmd = 'ESSID="'.$_POST['wifiName'].'" KEY_MGMT"'.$_POST['keyMgmt'].'" PROTO="'.$_POST['protocol'].'" PAIRWISE_CIPHERS="'.$_POST['pairWise'].'" GROUP_CIPHERS="'.$_POST['groupCiphers'].'" wifi-conf-create.sh';
+    $process = proc_open($cmd, $desc, $pipes);
 
-        //Input password, if defined
-        if(isset($_POST['password'])) {
-            fwrite($pipes[0], $_POST['password']);
-        }
-        //Close input to start command
-        fclose($pipes[0]);
-
-        $result = stream_get_contents($pipes[1]);
-
-        //Clean-up
-        fclose($pipes[1]);
-        fclose($pipes[2]);
-        proc_close($process);
-
-        header('Location: /wifi.php?result=' .  $result);
-        exit;
+    //Input password, if defined
+    if(isset($_POST['password'])) {
+        fwrite($pipes[0], $_POST['password']);
     }
+    //Close input to start command
+    fclose($pipes[0]);
+
+    $result = stream_get_contents($pipes[1]);
+
+    //Clean-up
+    fclose($pipes[1]);
+    fclose($pipes[2]);
+    proc_close($process);
+
+    header('Location: /wifi.php?result=' . $result);
+    exit;
+}
 ?>
 
     <div class="container">
