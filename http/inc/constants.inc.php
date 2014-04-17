27b58cd77d891ec0fe9871bbd209b41b2890d97e
Stas Oskin <stas.oskin@gmail.com>
2014-04-17 11:09:14 +0300

    Wifi WIP

--- http/inc/constants.inc.php
+++ http/inc/constants.inc.php
@@ -14,6 +14,8 @@ const MAX_ELECTRICAL_USAGE_FILE = '/etc/mg_psu_limit';
 const CURRENT_VERSION_FILE = '/fw_ver';
 const LATEST_VERSION_FILE = '/tmp/fw_update';
 
+const WIFI_NETWORKS_FILE = '/var/run/wifi';
+
 //leds
 //const GREEN_LED = 51; // reserved for miner use
 const YELLOW_LED = 22;
--- http/wifi.php
+++ http/wifi.php
@@ -5,10 +5,22 @@ include('head.php');
 include('menu.php');
 ?>
 
+<?php
+if (isset($_POST['wifiScan']))
+    header('Location: /wifi.php');
+
+    //Scan for WiFi on every page refresh
+    //exec("iwlist wlan0 scan | iwlist-scan-parse.awk > " . WIFI_NETWORKS_FILE); //TODO: Re-enable on miner
+    $wifiJson = file_get_contents(WIFI_NETWORKS_FILE);
+    $wifiNetworks = json_decode($wifiJson, true);
+?>
+
     <div class="container">
         <center><h1>WiFi networks</h1></center>
         <br/><br/>
-        <a class="btn btn-default" id="scan" name="scan" href='/'>Scan for WiFi</a>
+        <form name="wifi" action="/wifi.php" method="post" class="form-horizontal">
+        <button type="submit" class="btn btn-default">Scan for WiFi</button>
+        <input type='hidden' name='wifiScan'>
         <br><br>
         <table id="stats" class="tablesorter table table-striped table-hover stats">
             <thead>
@@ -21,11 +33,27 @@ include('menu.php');
             </thead>
             <tbody>
 
+            <?php
+            if(isset($wifiNetworks) && isset($wifiNetworks["WiFi"]) && sizeof($wifiNetworks["WiFi"]) > 0)
+                foreach($wifiNetworks["WiFi"] as $wifi){
+                    $connected = $wifi["Chan"] == 6;
+                    $connectedCSS = $connected ? "text-success" : "";
+                    ?>
+                    <tr>
+                        <td><span class="<?php echo $connectedCSS ?> "><?php echo $wifi["ESSID"]; echo $connected ? " (connected)" : "" ?></span></td>
+                        <td><i class="fa fa-signal fa-lg <?php echo $connectedCSS ?>"></i></td>
 
 
-            <tr>
+                        <td><i class="fa <?php echo $wifi["Enc"] ? "fa-lock" : "fa-unlock" ?> fa-lg <?php echo $connectedCSS ?>"></i></td>
 
-                <td><span class="text-success">Spond1</span></td>
+                        <td style="<?php echo !$connected ? "cursor: pointer;" : "" ?>"><i class="fa <?php echo $connected ? "fa-check-square" : "fa-link" ?> fa-lg <?php echo $connectedCSS ?>"></i></td>
+
+                    </tr>
+            <?php } ?>
+        </form>
+<!--            <tr>
+
+                <td><span class="text-success">Spond1 (connected)</span></td>
                 <td><i class="fa fa-signal fa-lg text-success"></i></td>
 
 
@@ -70,7 +98,7 @@ include('menu.php');
                 <td><i class="fa fa-link fa-lg text-info"></i></td>
 
             </tr>
-
+-->
             </tbody>
         </table>
     </div>
