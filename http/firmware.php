7759c1e66042394eca3e6c957727ce6c47440b3c
Stas Oskin <stas.oskin@gmail.com>
2014-05-12 04:30:42 +0300

    Extended to support advanced mode with version selection

--- http/firmware.php
+++ http/firmware.php
@@ -39,29 +39,79 @@ include('menu.php');
 
         }
 
+        advanced_warning = false;
+        function toggleCustomVersionSelection(force){
+            var advanced_warning;
+            var settingsView = $('#settings_view_name');
+
+            if (!force && !advanced_warning && settingsView.text() == "Advanced") {
+                advanced_warning = true;
+                bootbox.confirm("Choosing an outdated or test version might cause a lower perfromance for your miner or cause it to restart unexpectedly.<br/><br/>Do you want to continue?",
+                    function (confirm) {
+                        if (confirm) toggleCustomVersionSelection(true);
+                        else advanced_warning = false;
+                    });
+                return false;
+            }
+
+            $('.view-alternative').toggle();
+            settingsView.text(settingsView.text()=="Advanced"?"Basic":"Advanced");
+
+            return false;
+        }
     </script>
 
-    <div>
 
-        <div class="container">
-           <!-- <p class="alert"><b>WARNING:</b> Power interruption during the upgrade may brick your unit  and will require microSD restore procedure.</p> -->
-            <h1>Firmware upgrade</h1>
-            <p class="help-block">Your current firmware version is <b><?php echo(file_get_contents(CURRENT_VERSION_FILE)) ?></b></p>
-            <p class="help-block">Latest available firmware version is <b><?php echo(file_get_contents(LATEST_VERSION_FILE)) ?></b></p>
-            <br>
-            <a name="upgrade" class="btn btn-default miner-action" onclick="upgradeFirmware()">Upgrade Now</a>
+    <div class="container">
+        <!-- <p class="alert"><b>WARNING:</b> Power interruption during the upgrade may brick your unit  and will require microSD restore procedure.</p> -->
+        <h2>Settings</h2>
+
+        <fieldset>
+            <legend></legend>
+
+            <label for="selectedVersion">Current firmware version:</label>
+            <b><?php echo(file_get_contents(CURRENT_VERSION_FILE)) ?></b>
+            <br/>
+
+            <div class="basic view-alternative">
+                <label for="selectedVersion">Latest available firmware version:</label>
+                <b><?php echo(file_get_contents(LATEST_VERSION_FILE)) ?></b>
+            </div>
+
+            <div class="basic view-alternative" style="display:none">
+                <label for="selectedVersion">Available versions:</label>
+                <select class="form-control" id="selectedVersion">
+                    <?php
+                    $fwVersionsJson = file_get_contents(FIRMWARE_AVAILABLE_VERSIONS);
+                    $fwVersions = json_decode($fwVersionsJson, true);
+
+                    foreach($fwVersions as $fwVersion) {
+                        $testVersionLabel = $fwVersion["isTestVersion"] ? " - TEST VERSION" : "";
+                        echo('<option value="'. $fwVersion["firmwareVersion"] . '">' . $fwVersion["firmwareVersion"] . $testVersionLabel . '</option>');
+                    }
+                    ?>
+                </select>
+            </div>
+
+            <br/>
+            <div class="buttons">
+                <button class="btn btn-default" onclick="return upgradeFirmware()">Upgrade Now</button>
+
+                <button class="btn btn-default col-offset-2" onclick="return toggleCustomVersionSelection()"><span id="settings_view_name">Advanced</span> firmware selection</button>
+            </div>
+
             <br><br>
             <pre>
                 <div id="upgrade_output"></div>
                 <span id="upgrade_scroller"></span>
             </pre>
-        </div>
-    </div>
 
-    <center>
-        <br><br>
-        <a class="btn btn-default hidden" id="reboot" name="reboot" href='/reboot.php'>Reboot</a>
-    </center>
+            <div class="buttons">
+                <br><br>
+                <a class="btn btn-default hidden" id="reboot" name="reboot" href='/reboot.php'>Reboot</a>
+            </div>
+        </fieldset>
+    </div>
 <?php
 include('foot.php');
 
