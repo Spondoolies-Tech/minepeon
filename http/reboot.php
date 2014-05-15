4dadbaebaed3675bd438b7bc2e6ebbbbc2f6b73a
zvisha <zvisha@gmail.com>
2014-05-15 18:00:29 +0300

    1.3.17

--- http/reboot.php
+++ http/reboot.php
@@ -11,7 +11,7 @@ if(!isset($refresh_ip)) $refresh_ip =  $_SERVER['SERVER_ADDR'];
 <script type="text/javascript">
 var start = new Date();
 start = Date.parse(start)/1000;
-var seconds = 60;
+var seconds = 120;
 function CountDown(){
     var now = new Date();
     now = Date.parse(now)/1000;
@@ -23,7 +23,7 @@ function CountDown(){
 	    location.href = "http://<?php echo $refresh_ip?>/"
     }
 }
-window.setTimeout('CountDown()',60);
+window.setTimeout('CountDown()',100);
 </script>
 <?php } ?>
   </head>
@@ -32,7 +32,7 @@ window.setTimeout('CountDown()',60);
 <?php if($refresh_ip != "none"){ ?>
   <p><h1>Rebooting SP10 Dawson</h1></p>
   <p>You will be reconnected in</p>
-  <p><h1 id="countdown">60</h1></p>  
+  <p><h1 id="countdown">120</h1></p>
   <p>seconds.</p> 
 <?php } else{ ?>
 <div><center><h3>Your miner is rebooting.</h3><h5>If you changed your ip, your miner will have a new ip when it restarts. To access the MinerUI, enter the new IP into the address bar.</h5></center></div>
@@ -42,8 +42,8 @@ window.setTimeout('CountDown()',60);
 </html>
 <?php
 
-#exec('/usr/local/bin/spond-manager stop > /dev/null 2>&1 ');
-#sleep(3);
+exec('/usr/local/bin/spond-manager stop > /dev/null 2>&1 ');
+sleep(3);
 // flush buffer, so user can see the countdown timer without waiting for the script to complete (500 error)
 ob_flush();
 flush();
