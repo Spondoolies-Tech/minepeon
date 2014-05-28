1369e5c7a3d750300f63b407cb61ca2132043453
Stas Oskin <stas.oskin@gmail.com>
2014-05-28 05:01:37 +0300

    CGMiner reset

--- http/reset_cgminer_to_factory.php
+++ http/reset_cgminer_to_factory.php
@@ -4,8 +4,11 @@ require_once('miner.inc.php');
 //Remove the custom cgminer settings file
 unlink('/etc/cgminer.conf');
 
+//Write the default settings
+file_put_contents("/etc/cgminer.conf", '{"api-listen":true,"api-allow":"W:127.0.0.1","pools":[{"url":"stratum.btcguild.com:3333","user":"user","pass":"pass"}]}');
+
 //Restart CGMiner (by forcing service reload)
 $ret = miner_restart(false);
 
-//Reboot the machine
+//Reload the settings page
 header('Location: /settings.php');
