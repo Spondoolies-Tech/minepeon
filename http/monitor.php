f8b6ab9fbde429e3d0d869fc75d70d871c47d0f5
Simcha <simcha@spondoolies-tech.com>
2014-06-19 15:31:25 +0300

    added workmode data

--- http/monitor.php
+++ http/monitor.php
@@ -36,4 +36,8 @@ file_put_contents(MG_EVENTS_FILE, '');
 $json['mg_status'] = '';
 if(file_exists(MG_STATUS)) $json['mg_status'] = trim(file_get_contents(MG_STATUS));
 
+$json['workmode'] = array_combine(
+	array('fan_speed', 'min_voltage', 'max_voltage', 'max_watts', 'dc2dc_current'),
+	explode(' ', trim(file_get_contents(MINER_WORKMODE_FILE ))));
+
 echo json_encode($json);
