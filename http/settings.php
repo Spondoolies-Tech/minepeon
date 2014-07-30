22a32fcbfa71efab0d2a96b9b136652bf48b659c
Simcha <simcha@spondoolies-tech.com>
2014-07-30 12:13:03 +0300

    moved model-specific settings files to subdirectory

--- http/settings.php
+++ http/settings.php
@@ -5,4 +5,4 @@ require_once('miner.inc.php');
 require_once('network.inc.php');
 require_once('cron.inc.php');
 
-require_once('settings_'.$model_id.'.php');
+require_once($model_id.'/settings.php');
