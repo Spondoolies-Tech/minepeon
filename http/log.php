93efac1a87cc537fd4d2018dbf8373f63e25d196
Simcha <simcha@spondoolies-tech.com>
2014-08-21 14:31:02 +0300

    menu for log files

--- http/log.php
+++ http/log.php
@@ -3,6 +3,9 @@ require_once('global.inc.php');
 require('ansi.inc.php');
 include('head.php');
 include('menu.php');
+$log_file = 'messages';
+$log_id = $_GET['log'];
+if(!is_null($log_id) && $log_id != '') $log_file .= '.'.$log_id;
 ?>
 <h3>System events</h3>
 <pre style="padding:20px;font-size:85%">
@@ -16,15 +19,31 @@ $d="";
 <a class="btn btn-default clearlog" href='/control.php?op=clear_log'>Clear Events</a>
 
 <h3>Log</h3>
+<a href="?">Current</a>
+<?php 
+$h = opendir('/mnt/config/log/');
+$logs = array();
+while( ($file = readdir($h)) !== false){
+	$matches = array();
+	preg_match('/messages.(\d+)/', $file, $matches);
+	if(!empty($matches)){
+		$logs[] = $matches[1];
+	}
+}
+sort($logs);
+//$logs = array_slice($logs, 0, 21);
+foreach($logs as $l){
+	$style = '';
+	if($l == $log_id) $style = 'color:red;font-weight:bold;';
+	echo ' <a href="?log='.$l.'" style="'.$style.'">'.$l.'</a> ';
+}
+?>
 <pre style="padding:20px;font-size:85%">
 <div style="padding:10px;color:white;background:#282828">
+    
     <?php
-    exec('cat /mnt/config/log/messages.0',$d);
+    exec('cat /mnt/config/log/'.$log_file, $d);
     echo $ansi->convert(str_replace('^[', chr(27), implode("\n",$d)));
-    $d="";
-    exec('cat /mnt/config/log/messages',$d);
-    echo $ansi->convert(str_replace('^[', chr(27), implode("\n",$d)));
-    $d="";
     ?>
 </pre>
 
