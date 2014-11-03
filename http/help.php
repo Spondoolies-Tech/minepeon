4bf37bda3d337806e90505481fb2bc9c7e567fd1
alexspondoolies <alex@spondoolies-tech.com>
2014-11-03 14:27:20 +0200

    /etc/mg_* files explanation

--- http/help.php
+++ http/help.php
@@ -1,5 +1,4 @@
 <?php
-
 include('inc/global.inc.php');
 
 
@@ -7,28 +6,15 @@ include('inc/global.inc.php');
 include('head.php');
 include('menu.php');
 
+$file_content = file_get_contents("mg_etc_help");
 ?>
-        <div class="container">
-                <h1>License</h1> 
-				<div>
-				<p>There are many different components to MinePeon and you will find that they all have some sort of license, please respect their intellectual property rights by complying with their license, you will find the licence details included in the upstream package of the software.</p>
-
-				<p>The MinePeon software is Copyright 2013 Neil Fincham under the GPLv3.</p>
-<pre>
-    This software is free software: you can redistribute it and/or 
-    modify it under the terms of the GNU General Public License as 
-    published by the Free Software Foundation version 3.
-
-    This software is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see http://www.gnu.org/licenses/.
-</pre>
-				</div>
-        </div>
+<div class="container" >
+    <h1>Readme</h1> 
+    <div class="readme">
+        <pre>
+<?php echo $file_content; ?>
+        </pre>
+    </div>
+</div>
 <?php
-
 include('foot.php');
