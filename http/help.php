5fc7187caf153312acb568a0ce9cb81e634fe707
Remo <rimonl@gmail.com>
2015-02-05 17:25:58 +0200

    add hwtag to bottom of help tab

--- http/help.php
+++ http/help.php
@@ -1,12 +1,10 @@
 <?php
 include('inc/global.inc.php');
-
-
-
 include('head.php');
 include('menu.php');
 
 $file_content = file_get_contents("mg_etc_help");
+$hardware_tag = file_get_contents("/tmp/hwtag");
 ?>
 <div class="container" >
     <h1>Readme</h1> 
@@ -16,5 +14,14 @@ $file_content = file_get_contents("mg_etc_help");
         </pre>
     </div>
 </div>
+<div class="container" >
+    <h2>Hardware TAG</h2> 
+    <div class="hardware serials">
+        <pre>
+<?php echo $hardware_tag; ?>
+        </pre>
+    </div>
+</div>
 <?php
-include('foot.php');
+include('foot.php'); 
+?>
